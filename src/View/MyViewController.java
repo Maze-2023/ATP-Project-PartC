c9c66b49630bb70e830f97882c89a2748c125c9f
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-06-09 14:50:17 +0300

    new packages and modules

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,9 +1,9 @@
-package com.example.atpprojectpartc;
+package View;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MyViewController implements IView{
     @FXML
     private Label welcomeText;
 
