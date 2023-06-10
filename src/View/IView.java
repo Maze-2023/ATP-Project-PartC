3aef34f8728837f147511650f5049fa678295552
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-06-10 19:33:33 +0300

    Create IView according to instructions

--- src/View/IView.java
+++ src/View/IView.java
@@ -1,4 +1,15 @@
 package View;
 
+import ViewModel.MyViewModel;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public interface IView {
+    void setScene(Scene scene);
+    void setVM(MyViewModel myViewModel);
+    void setStage(Stage primaryStage);
+    void changeScreenSize(Scene scene);
+    void setPlayerIcon(String s) throws Exception;
+    void exit();
+
 }
