93777abbac4ffaa418a2095897fac131c06563d8
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-06-14 09:05:07 +0300

    main menu

--- src/View/HelloApplication.java
+++ src/View/HelloApplication.java
@@ -26,51 +26,51 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        IModel model = new MyModel();
-//        MyViewModel myViewModel = new MyViewModel(model);
-//        model.addToMe(myViewModel);
-//
-//        stage.setTitle("Welcome");
-//        FXMLLoader welcomeFXML = new FXMLLoader(getClass().getResource("Welcome.fxml"));
-//        Parent welcome = welcomeFXML.load();
-//        welcomeScene = new Scene(welcome,900,600);
-//        welcomeScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
-//        WelcomeController welcomeController = welcomeFXML.getController();
-//
-//        FXMLLoader playerFXML = new FXMLLoader(getClass().getResource("PlayerIcon.fxml"));
-//        Parent player = playerFXML.load();
-//        PlayerIconController playerController = playerFXML.getController();
-//        playerIconScene = new Scene(player,900,600);
-//        playerController.setStage(stage);
-//
-//
-//        FXMLLoader gameFXML = new FXMLLoader(getClass().getResource("MyView.fxml"));
-//        Parent game = gameFXML.load();
-//        MyViewController gameController = gameFXML.getController();
-//        gameScene = new Scene(game,900,600);
-//
-//        welcomeController.setStage(stage);
-//        welcomeController.setScene(playerIconScene);
-//
-//        playerController.setStage(welcomeController.getStage());
-//        playerController.setScene(gameScene);
-//
-//        playerController.setMyViewModel(myViewModel);
-//        //gameController.changeScreenSize(gameScene);
-//        playerController.setMyViewController(gameController);
-//
-//        myViewModel.addToMe(gameController);
-//
-//        stage.setScene(welcomeScene);
-//        stage.show();
+        IModel model = new MyModel();
+        MyViewModel myViewModel = new MyViewModel(model);
+        model.addToMe(myViewModel);
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Win.fxml"));
-        AnchorPane root = loader.load();
-        Scene scene = new Scene(root);
+        stage.setTitle("Welcome");
+        FXMLLoader welcomeFXML = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+        Parent welcome = welcomeFXML.load();
+        welcomeScene = new Scene(welcome,900,600);
+        welcomeScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+        WelcomeController welcomeController = welcomeFXML.getController();
 
-        stage.setScene(scene);
-        stage.setTitle("My Application");
+        FXMLLoader playerFXML = new FXMLLoader(getClass().getResource("PlayerIcon.fxml"));
+        Parent player = playerFXML.load();
+        PlayerIconController playerController = playerFXML.getController();
+        playerIconScene = new Scene(player,900,600);
+        playerController.setStage(stage);
+
+
+        FXMLLoader gameFXML = new FXMLLoader(getClass().getResource("MyView.fxml"));
+        Parent game = gameFXML.load();
+        MyViewController gameController = gameFXML.getController();
+        gameScene = new Scene(game,900,600);
+
+        welcomeController.setStage(stage);
+        welcomeController.setScene(playerIconScene);
+
+        playerController.setStage(welcomeController.getStage());
+        playerController.setScene(gameScene);
+
+        playerController.setMyViewModel(myViewModel);
+        //gameController.changeScreenSize(gameScene);
+        playerController.setMyViewController(gameController);
+
+        myViewModel.addToMe(gameController);
+
+        stage.setScene(welcomeScene);
         stage.show();
+
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("Win.fxml"));
+//        AnchorPane root = loader.load();
+//        Scene scene = new Scene(root);
+//
+//        stage.setScene(scene);
+//        stage.setTitle("My Application");
+//        stage.show();
 
     }
 
